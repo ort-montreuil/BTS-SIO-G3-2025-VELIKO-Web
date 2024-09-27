e9b04ec0bcd3a7ba9e4a266228286d0aa7ad403c
shirelmarciano05 <shirel.marciano05@gmail.com>
2024-09-27 09:26:13 +0200

    changement curl et de la boucle qui parcourt les stations et le nb de velos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,16 +10,17 @@ class HomeController extends AbstractController
 {
     # = attributs
     # @= annotations
-    #[Route('/')]
-    public function Home(): Response
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
     {
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json?=&=&=",
+            CURLOPT_PORT => "9042",
+            CURLOPT_URL =>"http://localhost:9042/api/stations",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 20,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_POSTFIELDS => "",
@@ -27,22 +28,23 @@ class HomeController extends AbstractController
         ]);
 
         $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
 
-        if ($response === false) {
-            var_dump(curl_error($curl));
+        if ($err) {
+            echo "cURL Error #:" . $err;
         } else {
-            // echo json_decode($response,associative: true);
             $response = json_decode($response, true);
-
         }
 
-        curl_close($curl);
+        //dd($response);
 
 
         $curl2 = curl_init();
 
         curl_setopt_array($curl2, [
-            CURLOPT_URL => "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json",
+            CURLOPT_PORT => "9042",
+            CURLOPT_URL => "http://localhost:9042/api/stations/status",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
@@ -54,22 +56,25 @@ class HomeController extends AbstractController
         ]);
 
         $status = curl_exec($curl2);
-        $err = curl_error($curl2);
+        $err2 = curl_error($curl2);
 
         curl_close($curl2);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
+        if ($err2) {
+            echo "cURL Error #:" . $err2;
         } else {
             $status = json_decode($status, true);
         }
 
+        //dd($response, $status);
+
+
         $stationData = [];//tableau vide pour inserer tt les donnees des deux urls
 
         //cette boucle parcourt chaque element dans le 1e tableau response (1er url)  et chaque element represente les infos de la station du 1er url
-        foreach ($response['data']['stations'] as $stationInfos) {
+        foreach ($response as $stationInfos) {
             //cette boucle parcourt chaque element dans le 2e tableau status(2e url) et chaque element represente les infos de status cad (2e url)
-            foreach ($status['data']['stations'] as $statusInfos) {
+            foreach ($status as $statusInfos) {
                 //si les numeros des stations des 2 urls sont les memes alors
                 if ($statusInfos['station_id'] == $stationInfos['station_id']) {
                     //on cree un tableau status data où l'on va stocker toutes les donnees qu'on a besoin que ce soit dans le 1e url ou 2e url
@@ -87,10 +92,9 @@ class HomeController extends AbstractController
             }
         }
 
+
         return $this->render('home/home.html.twig', [
             'titre' => '',
-            "response" => $response,
-            "status" => $status,
             "stationData" => $stationData,
         ]);
 
