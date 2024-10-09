bba60d01f087ede72acb530d78cd2281971bb051
shirelmarciano05 <shirel.marciano05@gmail.com>
2024-10-09 18:28:56 +0200

    password

---
+++ src/Form/UserPasswordType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password', PasswordType::class, [ // Ajout du champ pour le mot de passe actuel
+                'mapped'=> false,
+                'attr' => [
+                    'autocomplete' => 'current-password',
+                    'placeholder' => 'Ancien mot de passe',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre mot de passe actuel.',
+                    ]),
+                ],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Nouveau mot de passe',
+                    ],
+                    'label' => 'Nouveau password',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4',
+                    ],
+                    'constraints' => [ // Correction ici
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères.',
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Confirmez le mot de passe',
+                    ],
+                    'label' => 'Confirmation du password',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null, // Modifier pour ne pas mapper à l'entité User
+        ]);
+    }
+}
