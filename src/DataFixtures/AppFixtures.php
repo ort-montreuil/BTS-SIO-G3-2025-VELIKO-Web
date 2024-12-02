5beb9f82964e0094421de7a1ce4599d3c0eb634b
shirelmarciano05 <shirel.marciano05@gmail.com>
2024-12-02 11:14:28 +0100

    dataFixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+        // $this->hasher = $hasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+
+        //---------Notre ADMIN------------
+
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('toto@dev.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user,'Motdepasse123//'));
+        $user->setVerified(1);
+        $user->setDateNaissance(new \DateTime('1990-01-01'));
+        $user->setAdresse('1 rue de la rue');
+        $user->setVille('Paris');
+        $user->setCodePostal('75000');
+        $user->setNom('Toto');
+        $user->setPrenom('Mr');
+        $manager->persist($user);
+
+
+        $manager->flush();
+    }
+
+
+}
